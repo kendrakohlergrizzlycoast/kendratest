586d76cd017fc2d4712ba6d84be2d8ceb202436c
Kendra Kohler <kendra@grizzlycoastmedia.com>
2016-11-24 09:22:59 -0800

    testing session

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class IndexController extends Controller
 {
@@ -14,9 +14,13 @@ class IndexController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $session = $request->getSession();
+        
+        //$price = !$session->get('foo') ? 1 : $session->get('foo')+1;
+        $price = 1;
+        $session->set('foo',$price);
+        $foo = ['foo' => $session->get('foo'),'test' => 'BLAHBLAH'];
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/index.html.twig', $foo);
     }
 }
