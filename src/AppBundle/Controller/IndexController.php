24a5de9dcf26b9b78e93cf23ee280f33ba61acaf
Kendra Kohler <kendra@grizzlycoastmedia.com>
2016-11-24 09:20:14 -0800

    reverting

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Session;
+
 
 class IndexController extends Controller
 {
@@ -14,8 +14,6 @@ class IndexController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $session = $request->getSession();
-        
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
