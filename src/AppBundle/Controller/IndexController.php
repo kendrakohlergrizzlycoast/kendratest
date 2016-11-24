720fe563c43c946f1f41d8329d5a534abbde44da
Kendra Kohler <kendra@grizzlycoastmedia.com>
2016-11-24 09:01:09 -0800

    playing around with sessions

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class IndexController extends Controller
 {
@@ -13,6 +14,9 @@ class IndexController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $session = $request->getSession();
+        
+        $session->set('foo','bar');
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
--- web/app.php
+++ web/app.php
@@ -11,10 +11,6 @@ $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
-$session = new Session();
-$session->start();
-$session->set('name', 'foo');
-
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
